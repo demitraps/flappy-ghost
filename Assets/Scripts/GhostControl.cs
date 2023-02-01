46f4663365ff3c5fb50adb97ffa74057792d9213
Demitra <109766095+demitraps@users.noreply.github.com>
2023-02-01 18:33:53 +0200

    Update comments

--- Assets/Scripts/GhostControl.cs
+++ Assets/Scripts/GhostControl.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class GhostControl : MonoBehaviour
 {
+    //variable for accessing ghost's physics Rigidbody2D
     Rigidbody2D rb;
+    //variable for ghost's upward speed when clicked
+    //public so it can be accessed by Unity editor
     public float speed;
 
-    const float MIN_ANGLE = -7;
-    const float MAX_ANGLE = 5;
+    //constant variables for min and max angle rotation of ghost when going down or up
+    const float MIN_ANGLE = -9;
+    const float MAX_ANGLE = 7;
 
+    //angle of ghost when going up or down
     float angle = 0;
 
     //create a new object with class ScoreControl so we can call its method Scored()
@@ -25,6 +30,7 @@ public class GhostControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //get Component Rigidbody2D from ghost and assign it to rb at start
         rb = GetComponent<Rigidbody2D>();
         
     }
@@ -32,25 +38,33 @@ public class GhostControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //GetMouseButtonDown with 0 for right click/tap on screen
         if(Input.GetMouseButtonDown(0) && !GameManager.gameOver)//checks for tapping on screen & if the game isn't over
         {
+            //velocity of ghost: it accepts Vector2 values and we null the existing values each time screen is tapped
+            //new Vector2(0,0) is equal to Vector2.zero;
             rb.velocity = Vector2.zero;
+            //set velocity it to the original velocity on x, and speed(which is controlled on Editor) on y
             rb.velocity = new Vector2(rb.velocity.x, speed);
 
         }
+        //call Ghost Rotation method per frame
         GhostRotation();
     }
 
+    //method to control ghost's rotation
     void GhostRotation() 
     {
+        //positive velocity on y means the ghost goes upwards
         if(rb.velocity.y > 0)
-        {  if (angle <= MAX_ANGLE)
+        {  //as long as angle is smaller than the MAX_ANGLE we increase it by 0.5
+            if (angle <= MAX_ANGLE)
         {
             angle += 0.5f;
         }
-            
+          //negative velocity on y means the ghost goes downwards  
         } else if (rb.velocity.y < 0.5)
-        {
+        {   //as long as angle is bigger than the MIN_ANGLE we decrease it by 0.3f
             if (angle >= MIN_ANGLE)
             {
              angle -= 0.3f;
@@ -59,6 +73,7 @@ public class GhostControl : MonoBehaviour
         }
         if(!touchedGround)//rotate only if we have not touched the ground
         {
+            //apply angle on rotation of ghost
             transform.rotation = Quaternion.Euler(0,0,angle);
         }
 
--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 
 {
+    //variable speed of ground moving to the left
     float speed = 1;
+    //variable of ground's box collider so we can access its size on the x
     BoxCollider2D box;
+    //variable to assign the size
     float groundWidth;
 
     //variable for width of arm taken from radius of circle collider on arm
@@ -18,12 +21,14 @@ public class MoveLeft : MonoBehaviour
         //*MoveLeft is applied to multiple gameObjects
         if (gameObject.CompareTag("Ground"))
         {
+            //assign component of BoxCollider2D to box
             box = GetComponent<BoxCollider2D>();
-        groundWidth = box.size.x;
+            //assign size.x of box collider to groundWidth
+            groundWidth = box.size.x;
         } else if (gameObject.CompareTag("Arms"))
         {
             //use GameObject class's method FindGameObjectWithTag to isolate one of the two arms
-            //the get the Component's Circle Collider 2D with the GetComponent method and get the radius
+            //then get the Component's Circle Collider 2D with the GetComponent method and get the radius
             //assign the radius as armWidth when the game starts
             armWidth = GameObject.FindGameObjectWithTag("SingleArm").GetComponent<CircleCollider2D>().radius;
         }
@@ -36,6 +41,9 @@ public class MoveLeft : MonoBehaviour
         if(!GameManager.gameOver)//checks if the game is over
         {
             //moves items to the left
+            //position of item will be:
+            //on the x axis: the current position minus the speed(we are moving to the left)*Time.deltaTime
+            //on the y axis: the current position
         transform.position = new Vector2(
             transform.position.x - speed *Time.deltaTime, transform.position.y);
         }
@@ -43,8 +51,11 @@ public class MoveLeft : MonoBehaviour
 
             if(gameObject.CompareTag("Ground"))
             {
+                //return to original position
+                //check if it has moved past its size we want to move it back
                if(transform.position.x <= -groundWidth)
                {
+                //move it to the right two spots
                transform.position = new Vector2(transform.position.x + groundWidth*2, transform.position.y);
                } 
             } else if (gameObject.CompareTag("Arms"))
